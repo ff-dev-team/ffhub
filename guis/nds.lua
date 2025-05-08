2a704fe037742b0b77d1246bc4672b42922dbeac
timati_sensei <timati.other@gmail.com>
2025-05-08 20:20:16 +0300

    Add Loader and FFHub_NDS for testing purposes

---
+++ guis/nds.lua
@@ -0,0 +1,1 @@
+print("Loading FFHub for Natural Disaster Survival...")
---
+++ loader.lua
@@ -0,0 +1,9 @@
+local supportedGames = {
+    ["189707"] = "https://raw.githubusercontent.com/timatisensei/ffhub/refs/heads/main/guis/nds.lua"
+}
+local loadstr = supportedGames[str(game.PlaceId)]
+if loadstr == nil then
+    print("Game is not supported")
+    return
+end
+loadstring(game:HttpGet(loadstr))()
