f43402b3034f8364f8a378ff239050269da40cb2
timati_sensei <timati.other@gmail.com>
2025-05-08 20:27:41 +0300

    change loader a bit

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local supportedGames = {
     ["189707"] = "https://raw.githubusercontent.com/ff-dev-team/ffhub/refs/heads/main/guis/nds.lua"
 }
-local loadstr = supportedGames[str(game.PlaceId)]
+local loadstr = supportedGames[tostring(game.PlaceId)]
 if loadstr == nil then
     print("Game is not supported")
     return
